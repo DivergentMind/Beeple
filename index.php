8c950a06ba6c87e2b824b92e9a3e501a3134acde
jessiBramwell <bramibly@gmail.com>
2018-05-08 22:44:10 -0600

    added bones of homepage

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Beeple</title> 
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- FONT –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+        <!-- CSS –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/skeleton.css">
+    </head>
+    
+    <body>
+        <nav>
+            <ul>
+                <li><a>Graph</a></li>
+                <li><a>Table</a></li>
+            </ul>
+        </nav>
+        <div class="wrapper content">
+            <div>
+                <div class="inspection-btn">
+                    <button>New Inspection</button>    
+                </div>
+            </div>
+            
+            <div class="wrapper">
+                <div class="feed">
+                    <div class="recommend">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>       
+                    </div>
+                    <div class="actions">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>   
+                    </div>                    
+                    <div class="patterns">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>    
+                    </div>                              
+                </div>                
+            </div>            
+        </div>      
+    </body>
