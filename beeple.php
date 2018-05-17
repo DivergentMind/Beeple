0fb75957187545dae2921fb793a6d70d9cbd83d9
DivergentMind <38561985+DivergentMind@users.noreply.github.com>
2018-05-17 09:03:17 -0600

    Add files via upload
    Updated inspection sheet, functions and exported database

--- beeple.php
+++ beeple.php
@@ -97,23 +97,17 @@ if ( ! empty( $_POST ) ) {//print_r($_POST);
                 </div>
                 <div>
                     <h5>Hive Temperament</h5>
-                    <label><input type="radio" name="Temperament" value="Calm" checked>Calm</label>
+                    <label><input type="radio" name="Temperament" value="Calm">Calm</label>
                     <label><input type="radio" name="Temperament" value="Nervous" >Nervous</label>
                     <label><input type="radio" name="Temperament" value="Aggressive">Aggressive</label>
                     <label><input type="radio, hidden" name="Temperament" value="NA" checked hidden></label>
                 </div>
 
                 <div>
-                    <h5>Saw Queen</h5> <!-- this is a good spot for a colapsable form -->
+                    <h5>Saw Queen</h5> 
                     <label><input type="radio" name="QueenSeen" value="No" checked>No</label>
                     <label><input type="radio" name="QueenSeen" value="Yes">Yes</label>
-            <!--    <p>Marked?</p>
-                    <label><input type="radio" name="QueenMarked" value="Unknown" checked>Unknown</label>
-                    <label><input type="radio" name="QueenMarked" value="No" >No</label>
-                    <label><input type="radio" name="QueenMarked" value="Yes">Yes</label>
-                    <label>Color:<input type="text" name="QueenColor"></label>                    
-                    <label>Age of Queen: <input type="number" step="0.1" name="QueenAge" min="0" placeholder="Years"></label>
--->
+            
                 </div>
 
                 <div>
@@ -133,32 +127,11 @@ if ( ! empty( $_POST ) ) {//print_r($_POST);
 
                 <div>
                     <h5>Population</h5>
-                    <!--
-                    <label><input type="radio" name="Population" value="Stro">Strong</label>
-                    <label><input type="radio" name="Population" value="Mod">Moderate</label>
-                    <label><input type="radio" name="Population" value="Weak">Weak<br></label>
-                    <label><input type="radio" name="Population" value="NA" checked hidden></label>
-                    -->
-                    <label>Seams of Bees:<input type="number" name="Population" min="0" ></label><!-- This needs to be properly connected to database --> 
+                    <label>Seams of Bees:<input type="number" name="Population" min="0" ></label><br> 
                     <label>Crowded?<input type="radio" name="Crowded" value="Yes">Yes</label>
                     <label><input type="radio" name="Crowded" value="No" checked>No</label>                    
                 </div>
-<!--
-                <div>
-                    <h4>Excessive Drone Cells</h4>
-                    <label><input type="radio" name="ExcesiveDrone" value="No" checked>No</label>
-                    <label><input type="radio" name="ExcesiveDrone" value="Yes">Yes</label><br>                    
-                    <label>Comments:<input type="text" name="DroneComments"></label>
-                </div>
--->
- <!--               <div>
-                    <h4>Queen Cells</h4><!-- this is a good spot for a colapsable form --><!--
-                    <label><input type="radio" name="QueenCells" value="No" checked>No</label>
-                    <label><input type="radio" name="QueenCells" value="Yes">Yes:</label><br>    
-                    <label>Along frame bottom (swarm cells):#<input type="number" name="SwarmCellNum" min="0"></label><br>
-                    <label>Converted worker cell (supraceedure or emergency cells):#<input type="number" name="SupraCellNum" min="0"></label>
-                </div>
-        -->        
+
                 <div>
                     <h5>Food Stores</h5>
                     <label>Honey:<input type="radio" name="HoneyStores" value="Plenty">Plenty</label>                    
@@ -171,15 +144,7 @@ if ( ! empty( $_POST ) ) {//print_r($_POST);
                     <label><input type="radio, hidden" name="PollenStores" value="NA" checked hidden></label>
                 </div>
                 
-     <!--       <div> 
-                    <h4>Hive Condition</h4>
-                    <label><input type="checkbox" name="HiveCdns[Norm]" value="1">Normal</label>
-                    <label><input type="checkbox" name="HiveCdns[BraceComb]" value="1">Brace Comb</label>
-                    <label><input type="checkbox" name="HiveCdns[ExProp]" value="1">Excessive Propolis</label>
-                    <label><input type="checkbox" name="HiveCdns[Stink]" value="1">Foul odor</label>
-                    <label><input type="checkbox" name="HiveCdns[Damage]" value="1">Equip. Damage</label>
-                    <label><input type="checkbox" name="HiveCdns[Other]" value="1">Other:</label><input type="text" name="HiveCdnOtherText">
-                </div>-->
+     
 
                 <div>
                     <h5>Disease/Pests/Death</h5>                    
@@ -228,42 +193,6 @@ if ( ! empty( $_POST ) ) {//print_r($_POST);
                     <label><input type="radio" name="Dead" value="Yes">Yes</label><br>   
                     <label>Suspected Cause:<input type="text" name="DeadComments"></label>                
                 </div>           
-                    
-          <!--  <div>
-                    <h4>Actions Taken</h4>                    
-                    <label><input type="checkbox" name="Actions[FedSugar]" value="1">Fed Hive (Suryp)</label>                    
-                    <label><input type="checkbox" name="Actions[FedPollen]" value="1">Fed Hive (Pollen Patty)</label>
-                    <label><input type="checkbox" name="Actions[Excluder]" value="1">Added Excluder</label>
-                    <label><input type="checkbox" name="Actions[Requeen]" value="1">Requeened</label>
-                    <label><input type="checkbox" name="Actions[SwapBox]" value="1">Swapped Brood Boxes</label>
-                    <label><input type="checkbox" name="Actions[RemoveComb]" value="1">Removed Old Comb:</label>
-                    <label>How many frames?<input type="number" name="ActionsRemoveCombNumber" min="0"></label>
-                    <label><input type="checkbox" name="Actions[HoneyHarvest]" value="1">Honey Harvested:</label>
-                    <label>How many Pounds?<input type="number" step="0.01" min="0" name="ActionsHoneyHarvNumber"></label>
-                    <label><input type="checkbox" name="Actions[AddBox]" value="1">Added Box</label>
-                    <label><input type="checkbox" name="Actions[Split]" value="1">Split Hive:</label>
-                    <label>New Hive #<input type="text" name="ActionsNewHiveNum"></label>
-                    <label><input type="checkbox" name="Actions[Merge]" value="1">Merged Hive:</label>
-                    <label>Merged Hive #<input type="text" name="ActionsMergedHiveNum"></label>
-                    <label><input type="checkbox" name="Recs[ReplaceEquip]" value="1">Replaced Equipment</label>
-                    <label><input type="checkbox" name="Actions[Other]" value="1">Other:</label><input type="text" name="ActionsOtherText">                    
-                </div>-->
-                    
-        <!--    <div>
-                    <h4>Recommendations:</h4>
-                    <label><input type="checkbox" name="Recs[FedSugar]" value="1">Feed Hive (Suryp)</label>                    
-                    <label><input type="checkbox" name="Recs[FedPollen]" value="1">Feed Hive (Pollen Patty)</label>
-                    <label><input type="checkbox" name="Recs[Excluder]" value="1">Add Excluder</label>
-                    <label><input type="checkbox" name="Recs[AddBox]" value="1">Add Box</label>
-                    <label><input type="checkbox" name="Recs[Split]" value="1">Split</label>
-                    <label><input type="checkbox" name="Recs[Merge]" value="1">Merge Hive:</label>
-                    <label><input type="checkbox" name="Recs[ReplaceQueen]" value="1">Replace Queen</label>
-                    <label><input type="checkbox" name="Recs[SwarmWatch]" value="1">Swarming Imminent - Needs Monitoring</label>
-                    <label><input type="checkbox" name="Recs[ReplaceEquip]" value="1">Replace Equipment:<input type="text" name="RecReplaceEquipText"></label>                   
-                    <label><input type="checkbox" name="Recs[HoneyHarvest]" value="1">Honey Harvest:</label>
-                    <label><input type="checkbox" name="Recs[Other]" value="1">Other:</label><input type="text" name="RecOtherText"><br>                    
-                    <label>Comments:<input type="text" name="RecComments"></label>
-                </div>-->
 
                 <div>                
                     <label>General Comments:<input type="text" name="GenComments"></label><br>
