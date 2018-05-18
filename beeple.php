d4491ad65f745666ba884e72c4e0349688d51884
jessiBramwell <bramibly@gmail.com>
2018-05-18 09:41:30 -0600

    minor experiments with tasklist

--- beeple.php
+++ beeple.php
@@ -58,9 +58,12 @@ if ( ! empty( $_POST ) ) {//print_r($_POST);
                     <label><input type="radio" name="Temperament" value="Aggressive">Aggressive</label>
                     <label><input type="radio, hidden" name="Temperament" value="NA" checked hidden></label>
                 </div>
+                <hr />
+                    
+                <h4>Queen and Brood</h4>
 
                 <div>
-                    <h5>Saw Queen</h5> <!-- this is a good spot for a colapsable form -->
+                    <h5>Saw Queen</h5>
                     <label><input type="radio" name="QueenSeen" value="No" checked>No</label>
                     <label><input type="radio" name="QueenSeen" value="Yes">Yes</label>
             <!--    <p>Marked?</p>
@@ -84,9 +87,11 @@ if ( ! empty( $_POST ) ) {//print_r($_POST);
                     <h6></h6>
                     <label>Eggs Seen:<input type="radio" name="EggsSeen" value="Yes">Yes</label>
                     <label><input type="radio" name="EggsSeen" value="No" checked>No</label><br>
-                    <label>Comments:<input type="text" name="EggComments"></label>
-                    <hr />
+                    <label>Comments:<input type="text" name="EggComments"></label>                    
                 </div>
+                    
+                <hr />   
+                <h4>Hive Condition</h4>
 
                 <div>
                     <h5>Population</h5>
@@ -137,54 +142,63 @@ if ( ! empty( $_POST ) ) {//print_r($_POST);
                     <label><input type="checkbox" name="HiveCdns[Damage]" value="1">Equip. Damage</label>
                     <label><input type="checkbox" name="HiveCdns[Other]" value="1">Other:</label><input type="text" name="HiveCdnOtherText">
                 </div>-->
-
+                <hr />
+                <h4>Disease/Pests/Death</h4>
                 <div>
-                    <h5>Disease/Pests/Death</h5>                    
-                    <h5>Verroa mites</h5><!-- this is a good spot for a colapsable form -->
+                    <button type="button" id="MiteTreatBtn">Recommend Mite Treatment</button>    
+                </div>
+                <div>                    
+                    <h5>Verroa mites</h5>
                     <label>Mite Check:<input type="radio" name="MiteCheck" value="No" checked>No</label>
                     <label><input type="radio" name="MiteCheck" value="Yes">Yes</label><br>                    
                     <label><input type="radio" name="SamplingMethod" value="Sugar" checked>Sugar Roll</label>                 
                     <label><input type="radio" name="SamplingMethod" value="Alcohol">Alcohol Wash</label>
                     <label><input type="radio" name="SamplingMethod" value="NA" checked>N/A<br></label>
                     <label>How many mites in the sample?<input type="number" name="MiteCount" min="0" value="null"></label><br>
-                    <button type="button" id="MiteTreatBtn">Recommend Mite Treatment</button>
+                   <!-- <button type="button" id="MiteTreatBtn">Recommend Mite Treatment</button> -->
+                </div> 
                     
-                    <div id="MiteModal"class="modal">
-                        <div class="MiteTreat">
-                            <span class="close">&times;</span>
-                            <label>Mite Treatment:<input type="radio" name="MiteTreat" value="No" checked>No</label><!-- this is a good spot for a colapsable form -->
-                            <label><input type="radio" name="MiteTreat" value="Yes">Yes</label><br>
-                            <label><input type="radio" name="MiteTreatType" value="NA" checked hidden></label> 
-                            <label><input type="radio" name="MiteTreatType" value="MiteTreatOils">Essential Oils</label>                    
-                            <label><input type="radio" name="MiteTreatType" value="MiteTreatApivar">Apivar</label>                    
-                            <label><input type="radio" name="MiteTreatType" value="MiteTreatApistan">Apistan or Checkmate+</label>                    
-                            <label><input type="radio" name="MiteTreatType" value="MiteTreatFAcid">Formic Acid</label>                    
-                            <label><input type="radio" name="MiteTreatType" value="MiteTreatOxAcid">Oxalic Acid</label>                    
-                            <label><input type="radio" name="MiteTreatType" value="MiteTreatHops">Hop Guard</label>                    
-                            <label><input type="radio" name="MiteTreatType" value="MiteTreatOther">Other:</label><input type="text" name="MiteTreatOtherText"><br>               
-                            <label>When do treatments need to be tended/removed?:<input type="date" name="TreatRemoveDate"></label>
-                            <input type="hidden" name="MiteTreatType" value="NA" checked><!--Hide--> 
-                        </div>
+                <div id="MiteModal" class="modal">
+                    <div class="MiteTreat">
+                        <span class="close">&times;</span>
+                        <label>Mite Treatment:<input type="radio" name="MiteTreat" value="No" checked>No</label><!-- this is a good spot for a colapsable form -->
+                        <label><input type="radio" name="MiteTreat" value="Yes">Yes</label><br>
+                        <label><input type="radio" name="MiteTreatType" value="NA" checked hidden></label> 
+                        <label><input type="radio" name="MiteTreatType" value="MiteTreatOils">Essential Oils</label>                    
+                        <label><input type="radio" name="MiteTreatType" value="MiteTreatApivar">Apivar</label>                    
+                        <label><input type="radio" name="MiteTreatType" value="MiteTreatApistan">Apistan or Checkmate+</label>                    
+                        <label><input type="radio" name="MiteTreatType" value="MiteTreatFAcid">Formic Acid</label>                    
+                        <label><input type="radio" name="MiteTreatType" value="MiteTreatOxAcid">Oxalic Acid</label>                    
+                        <label><input type="radio" name="MiteTreatType" value="MiteTreatHops">Hop Guard</label>                    
+                        <label><input type="radio" name="MiteTreatType" value="MiteTreatOther">Other:</label><input type="text" name="MiteTreatOtherText"><br>               
+                        <label>When do treatments need to be tended/removed?:<input type="date" name="TreatRemoveDate"></label>
+                        <input type="hidden" name="MiteTreatType" value="NA" checked><!--Hide--> 
                     </div>
+                </div>
                     
-                    
-                    <h5>Other Problems</h5>
-                    <label><input type="checkbox" name="OtherProbs[ChalkBrood]" value="1">Chalk Brood</label>
-                    <label><input type="checkbox" name="OtherProbs[Nos]" value="1">Nosema</label>
-                    <label><input type="checkbox" name="OtherProbs[EFB]" value="1">E. Foulbrood</label>
-                    <label><input type="checkbox" name="OtherProbs[AFB]" value="1">A. Foulbrood</label>
-                    <label><input type="checkbox" name="OtherProbs[Beetle]" value="1">Hive Beetle</label>
-                    <!-- Add wax moth -->
-                    <label><input type="checkbox" name="OtherProbs[Other]" value="1">Other:<input type="text" name="OtherProbOtherText"></label><br>
-                    <input type="hidden" name="OtherProbs[NA]" value="0">
-                    <label>Treatments:<input type="text" name="OtherProbTreat"></label><br>                    
-                    <label>Comments:<input type="text" name="OtherProbComments"></label>
-                    
+                <div class="hideToggle">
+                    <h5 class="hide">Other Problems</h5>
+                    <div class="hidden">
+                        <label><input type="checkbox" name="OtherProbs[ChalkBrood]" value="1">Chalk Brood</label>
+                        <label><input type="checkbox" name="OtherProbs[Nos]" value="1">Nosema</label>
+                        <label><input type="checkbox" name="OtherProbs[EFB]" value="1">E. Foulbrood</label>
+                        <label><input type="checkbox" name="OtherProbs[AFB]" value="1">A. Foulbrood</label>
+                        <label><input type="checkbox" name="OtherProbs[Beetle]" value="1">Hive Beetle</label>
+                        <!-- Add wax moth -->
+                        <label><input type="checkbox" name="OtherProbs[Other]" value="1">Other:<input type="text" name="OtherProbOtherText"></label><br>
+                        <input type="hidden" name="OtherProbs[NA]" value="0">
+                        <label>Treatments:<input type="text" name="OtherProbTreat"></label><br>                    
+                        <label>Comments:<input type="text" name="OtherProbComments"></label>
+                    </div>
+                </div>
+                <div class="hideToggle">
                     <h5>Dead Hive</h5><!-- this is a good spot for a colapsable form -->
-                    <label><input type="radio" name="Dead" value="No" checked>No</label>
-                    <label><input type="radio" name="Dead" value="Yes">Yes</label><br>   
-                    <label>Suspected Cause:<input type="text" name="DeadComments"></label>                
-                </div>           
+                    <div class="hidden">                        
+                        <label><input type="radio" name="Dead" value="No" checked>No</label>
+                        <label><input type="radio" name="Dead" value="Yes">Yes</label><br>   
+                        <label>Suspected Cause:<input type="text" name="DeadComments"></label>   
+                    </div>
+                </div>         
 
           <!--  <div>
                     <h4>Actions Taken</h4>                    
